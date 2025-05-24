560059ba6767d6ae56b6c07e4ae5a43faa131c07
Hadi Nofal <anofal@ejada.com>
2025-05-24 17:04:08 +0300

    init

--- starter/backend/movies/app.py
+++ starter/backend/movies/app.py
@@ -1,14 +1,24 @@
 # starter/backend/movies/app.py
 from flask import Flask, jsonify
 from flask_cors import CORS
-from movies.resources import Movies  # Absolute import
+from flask.views import MethodView
+from movies.resources import Movies
+
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/movies')
-def get_movies():
-    return jsonify(movies=Movies)
+
+class MoviesAPI(MethodView):
+    def get(self, movie_id):
+        if movie_id is None:
+            return jsonify(movies=Movies)
+        else:
+            return jsonify(movie=Movies[int(movie_id)])
+
+
+app.add_url_rule('/movies', view_func=MoviesAPI.as_view('movies'))
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # Ensure newline at end
